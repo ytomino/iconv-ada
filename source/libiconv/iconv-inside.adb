d172ef1fc5f1c1123fc5239c50c8af54a8117927
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-05-20 18:22:17 +0900

    Do not use Import for conversions with suppressing their initializations

--- source/libiconv/iconv-inside.adb
+++ source/libiconv/iconv-inside.adb
@@ -1,3 +1,4 @@
+with Ada.Unchecked_Conversion;
 with C.iconv;
 with C.string;
 package body iconv.Inside is
@@ -16,21 +17,29 @@ package body iconv.Inside is
 		data : C.void_ptr)
 		return C.signed_int
 	is
-		Process : access procedure (Name : in String)
-			with Import;
-		for Process'Address use System.Address (data);
-		Names_Array : array (1 .. namescount) of C.char_const_ptr
-			with Import;
-		for Names_Array'Address use names.all'Address;
+		type Process_Type is access procedure (Name : in String);
+		function To_Process is
+			new Ada.Unchecked_Conversion (C.void_ptr, Process_Type);
+		type char_const_ptr_arrayN is
+			array (1 .. namescount) of C.char_const_ptr
+			with Convention => C;
+		type char_const_ptr_arrayN_const_ptr is
+			access constant char_const_ptr_arrayN
+			with Convention => C;
+		function To_char_const_ptr_arrayN_const_ptr is
+			new Ada.Unchecked_Conversion (
+				C.char_const_ptr_ptr,
+				char_const_ptr_arrayN_const_ptr);
+		Names_Array : char_const_ptr_arrayN
+			renames To_char_const_ptr_arrayN_const_ptr (names).all;
 	begin
 		for I in 1 .. namescount loop
 			declare
 				Length : constant Natural := Natural (C.string.strlen (Names_Array (I)));
-				Name : String (1 .. Length)
-					with Import;
+				Name : String (1 .. Length);
 				for Name'Address use Names_Array (I).all'Address;
 			begin
-				Process (Name);
+				To_Process (data) (Name);
 			end;
 		end loop;
 		return 0;
@@ -52,8 +61,11 @@ package body iconv.Inside is
 	end Version;
 	
 	procedure Iterate (Process : not null access procedure (Name : in String)) is
+		type Process_Type is access procedure (Name : in String);
+		function To_void_ptr is
+			new Ada.Unchecked_Conversion (Process_Type, C.void_ptr);
 	begin
-		C.iconv.iconvlist (Do_One'Access, C.void_ptr (Process'Address));
+		C.iconv.iconvlist (Do_One'Access, To_void_ptr (Process));
 	end Iterate;
 	
 end iconv.Inside;
